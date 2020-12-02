c089d25e1bb8ed673c0e346b19a736c2ecefb4ef
JoieHuiju <joie.huiju@gmail.com>
2020-12-02 18:24:22 +0900

    [Update] Do_it > chap6

---
+++ Do_it/chap06/insertion_sort.py
@@ -0,0 +1,27 @@
+# 단순 삽입 정렬 알고리즘 구현하기
+
+from typing import MutableSequence
+
+def insertion_sort(a: MutableSequence) -> None:
+    """단순 삽입 정렬"""
+    n = len(a)
+    for i in range(1, n):
+        j = i
+        tmp = a[i]
+        while j > 0 and a[j-1] > tmp:
+            a[j] = a[j-1]
+            j -= 1
+        a[j] = tmp
+
+if __name__ == '__main__':
+    num = int(input('원소 수를 입력하세요. : '))
+    x = [None] * num # 원소 수가 num인 배열을 생성
+
+    for i in range(num):
+        x[i] = int(input(f'x[{i}]: '))
+    
+    insertion_sort(x)
+
+    print('오름차순으로 정렬 완료')
+    for i in range(num):
+        print(f'x[{i}] = {x[i]}')
