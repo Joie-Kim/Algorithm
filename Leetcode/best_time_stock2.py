849d72d58bc8ff59baeeb42d418c29b749822108
JoieHuiju <joie.huiju@gmail.com>
2021-03-07 21:43:14 +0900

    [Add] Leetcode > 122. Best Time to Buy and Sell Stock II

---
+++ Leetcode/best_time_stock2.py
@@ -0,0 +1,15 @@
+# 122. Best Time to Buy and Sell Stock II
+# You are given an array prices where prices[i] is the price of a given stock on the ith day.
+# Find the maximum profit you can achieve. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+# Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+# 64 ms
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        answer = 0
+        
+        for i in range(len(prices) - 1):
+            if prices[i+1] > prices[i]:
+                answer += prices[i+1] - prices[i]
+        
+        return answer
