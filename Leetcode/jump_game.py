dd01e11aa7376f163ee596c193df49d1fa92e617
Joie-Kim <joie.huiju@gmail.com>
2022-10-11 23:41:41 +0900

    [Add] Leetcode > 55. Jump Game

---
+++ Leetcode/jump_game.py
@@ -0,0 +1,15 @@
+# 55. Jump Game
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        max_distance = 0
+        i = 0
+        
+        while (i <= max_distance):
+            print(i)
+            max_distance = max(max_distance, i + nums[i])
+            if (max_distance >= len(nums) - 1):
+                return True
+            i += 1
+        
+        return False
